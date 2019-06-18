7f12c8c747dd72f21b8b6bbeb1327b82d84b36cd
jainaditya21 <jain.aditya@curefit.com>
2019-06-18 10:06:39 +0530

    heroku changes

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ const app = express();
 
 app.use('/node_modules', express.static('node_modules'));
 app.use('/', express.static('app'));
-app.listen(3000, () => {
-    console.log('Express server running at http://127.0.0.1:3000')
+
+let listenPort = process.env.PORT || 3000;
+
+app.listen(listenPort, () => {
+    console.log('Express server running at port: ' + listenPort)
 });
 app.get('/', function (req, res) {
     res.send('Hello World!')
