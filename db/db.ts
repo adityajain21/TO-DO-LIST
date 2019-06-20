b2920a941c1b8e367db23012318e92513a8835f9
jainaditya21 <jain.aditya@curefit.com>
2019-06-20 20:01:15 +0530

    added end points to add, delete, fetch todo with local db

---
+++ db/db.ts
@@ -0,0 +1,9 @@
+const todos =  [
+    {
+        id: 1,
+        title: "lunch",
+        description: "Go for lunc by 2pm"
+    }
+];
+
+export default todos;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import todos from "../db/db";
+import bodyParser from 'body-parser';
 const app = express();
 
 app.use('/node_modules', express.static('node_modules'));
 app.use('/', express.static('app'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 let listenPort = process.env.PORT || 3000;
 
@@ -11,4 +15,67 @@ app.listen(listenPort, () => {
 });
 app.get('/', function (req, res) {
     res.send('Hello World!')
+})
+
+app.get('/v1/todos', (req, res) => {
+    res.send(200).send(todos.toString())
+})
+
+app.post('/v1/todos', (req, res) => {
+    if(!req.body.title) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    } else if(!req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+    const todo = {
+        id: todos.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    todos.push(todo);
+    return res.status(201).send({
+        message: 'todo added successfully',
+        todo
+    })
+})
+
+
+app.get('/v1/todos/:id', (req, res) => {
+
+    const id = parseInt(req.params.id, 10);
+    todos.map((todo) => {
+
+        if (todo.id === id) {
+            return res.status(200).send({
+                message: 'todo retrieved successfully',
+                todo,
+            });
+        }
+    });
+    return res.status(404).send({
+        message: 'todo does not exist',
+    });
+})
+
+app.delete('/v1/todos/:id', (req, res) => {
+
+    const id = parseInt(req.params.id, 10);
+    todos.map((todo, index) => {
+        if (todo.id === id) {
+            todos.splice(index, 1);
+            return res.status(200).send({
+                message: 'Todo deleted successfuly',
+            });
+        }
+    });
+
+    return res.status(404).send({
+        message: 'todo not found',
+    });
 })
